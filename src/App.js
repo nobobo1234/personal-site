832f56fb078dcba4a4b9f9c3a1cfa0bec5b2a1f3
nobobo1234 <noahvanboven@gmail.com>
2019-02-26 16:42:51 +0000

    Adding website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        # Navbar with nice logo (cta button)
+        # Header (introduction) (cta button)
+        # About Me with photo (resume)
+        # Speciality
+        # Portfolio
+        # Contact me
+        # Footer with social account and contact information
+      </div>
+    );
+  }
+}
+
+export default App;
