4161d787e083a57ef88d0cee7eb51ac53a887a2c
nola1222 <noahvanboven@gmail.com>
2019-03-01 21:49:35 +0100

    Improving on the design

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,10 @@ import styles from './Contact.module.scss';
 const contact = props => {
   return (
     <section className={styles.contact}>
-      <h2 className="heading--2">Contact me</h2>
+      <h2 className="heading--3 heading--3--dark">Contact me</h2>
+      <p className={styles['contact__text']}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut cumque debitis distinctio doloribus eveniet facilis fugiat maxime mollitia nobis, ratione repudiandae? Consequuntur, dolores expedita incidunt laborum magnam maiores odio!
+      </p>
     </section>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const header = props => {
   return (
     <header className={styles.header}>
       <h1 className="heading--1">My name</h1>
-      <h3 className="heading--3">Junior Front-end Web Developer</h3>
+      <h3 className="heading--3 heading--3--light">Junior Front-end Web Developer</h3>
       <div className={styles['header__buttons']}>
         <a href="/" className="btn btn--light">About me</a>
         <a href="/" className="btn btn--cta">Contact me</a>
